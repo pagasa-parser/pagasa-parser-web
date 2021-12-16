e755895d290ea261f099e0cdec62fc5d0079dda0
ChlodAlejandro <chlod@chlod.net>
2021-12-17 00:12:54 +0800

    we go

--- rebuild.sh
+++ rebuild.sh
@@ -1,5 +1,5 @@
 docker container stop pagasa-parser-web
 docker container rm pagasa-parser-web
 docker rmi pagasa-parser-web
-docker build -t pagasa-parser-web .
-docker run -d --name pagasa-parser-web -p 12464:12464 --restart unless-stopped pagasa-parser-web
+docker build -t pagasa-parser-web . \
+&& docker run -d --name pagasa-parser-web -p 12464:12464 --restart unless-stopped pagasa-parser-web
