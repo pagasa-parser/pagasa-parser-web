8a788fc2523f28ac6ee08a23336c5639d4b246b2
ChlodAlejandro <chlod@chlod.net>
2021-12-17 10:57:56 +0800

    fancy scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,12 @@
-cd ..
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+cd $SCRIPTPATH/..
 
 VERSION=`cat .version | tr -d "[:space:]"`
 
 docker container stop pagasa-parser-web
 docker container rm pagasa-parser-web
-docker build -t "chlod/pagasa-parser-web:latest" -t "chlod/pagasa-parser-web:`$VERSION`" . && \
- docker run -d --name pagasa-parser-web -p 12464:12464 --restart unless-stopped chlod/pagasa-parser-web:latest
+set -euxo pipefail
+docker build -t "chlod/pagasa-parser-web:latest" -t "chlod/pagasa-parser-web:$VERSION" .
+docker run -d --name pagasa-parser-web -p 12464:12464 --restart unless-stopped chlod/pagasa-parser-web:latest
