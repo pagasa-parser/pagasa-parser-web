921b7e8cdfe279cfd406f9d530c7a897294a32b4
ChlodAlejandro <chlod@chlod.net>
2022-01-23 06:32:00 +0800

    fix version script

--- scripts/versions.js
+++ scripts/versions.js
@@ -1,7 +1,7 @@
 const path = require("path");
 module.exports = {
-    backendPackagePath: path.resolve("../backend/package.json"),
-    frontendPackagePath: path.resolve("../frontend/package.json"),
-    backendPackageLockPath: path.resolve("../backend/package-lock.json"),
-    frontendPackageLockPath: path.resolve("../frontend/package-lock.json"),
+    backendPackagePath: path.resolve(__dirname, "..", "backend", "package.json"),
+    frontendPackagePath: path.resolve(__dirname, "..", "frontend", "package.json"),
+    backendPackageLockPath: path.resolve(__dirname, "..", "backend", "package-lock.json"),
+    frontendPackageLockPath: path.resolve(__dirname, "..", "frontend", "package-lock.json"),
 };
