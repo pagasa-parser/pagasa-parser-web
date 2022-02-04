b2d2407973150188dca160952dcae16e4fc68a3a
ChlodAlejandro <chlod@chlod.net>
2022-02-04 16:01:22 +0800

    change version script

---
+++ scripts/build-and-run.sh
@@ -0,0 +1,2 @@
+source build.sh
+docker run -d --name pagasa-parser-web -p 12464:80 chlod/pagasa-parser-web:latest
--- scripts/versions.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-module.exports = {
-    backendPackagePath: path.resolve(__dirname, "..", "backend", "package.json"),
-    frontendPackagePath: path.resolve(__dirname, "..", "frontend", "package.json"),
-    backendPackageLockPath: path.resolve(__dirname, "..", "backend", "package-lock.json"),
-    frontendPackageLockPath: path.resolve(__dirname, "..", "frontend", "package-lock.json"),
-};
